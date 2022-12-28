d09aa60acf4b3bf61eb34bdf62b6f2042c7d1729
David Vera Castillo <indigodavid@gmail.com>
2022-12-28 13:13:22 -0500

    ✅ Add base query structure as schema

---
+++ server/schema/schema.js
@@ -0,0 +1,32 @@
+const { projects, clients } = require('../sampleData');
+
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');
+
+// ClientType
+
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return clients.find(client => client.id === args.id);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
