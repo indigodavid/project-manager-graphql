f66d3b26c437b302af37b537e51cbd6a61a762ae
David Vera Castillo <indigodavid@gmail.com>
2022-12-28 13:12:06 -0500

    ✅ Add sample data

---
+++ server/sampleData.js
@@ -0,0 +1,60 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'EzCar',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit adipisci vel consequatur impedit nihil minima cumque labore dolores, ducimus repellat autem quas, repellendus deserunt ratione iure dolor non doloribus soluta. Saepe tenetur odit aut numquam non eos? Nobis alias, suscipit, ipsam sunt unde eligendi eius, velit quae maiores qui natus.',
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'EzPay',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit adipisci vel consequatur impedit nihil minima cumque labore dolores, ducimus repellat autem quas, repellendus deserunt ratione iure dolor non doloribus soluta. Saepe tenetur odit aut numquam non eos? Nobis alias, suscipit, ipsam sunt unde eligendi eius, velit quae maiores qui natus.',
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '1',
+    name: 'Handyman',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit adipisci vel consequatur impedit nihil minima cumque labore dolores, ducimus repellat autem quas, repellendus deserunt ratione iure dolor non doloribus soluta. Saepe tenetur odit aut numquam non eos? Nobis alias, suscipit, ipsam sunt unde eligendi eius, velit quae maiores qui natus.',
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '3',
+    name: 'Blog App',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit adipisci vel consequatur impedit nihil minima cumque labore dolores, ducimus repellat autem quas, repellendus deserunt ratione iure dolor non doloribus soluta. Saepe tenetur odit aut numquam non eos? Nobis alias, suscipit, ipsam sunt unde eligendi eius, velit quae maiores qui natus.',
+    status: 'In Progress',
+  },
+  {
+    id: '5',
+    clientId: '3',
+    name: 'Math Magicians',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit adipisci vel consequatur impedit nihil minima cumque labore dolores, ducimus repellat autem quas, repellendus deserunt ratione iure dolor non doloribus soluta. Saepe tenetur odit aut numquam non eos? Nobis alias, suscipit, ipsam sunt unde eligendi eius, velit quae maiores qui natus.',
+    status: 'In Progress',
+  },
+];
+
+const clients = [
+  {
+    id: '1',
+    name: 'David Vera',
+    email: 'david@gmail.com',
+    phone: '+593 986999845',
+  },
+  {
+    id: '2',
+    name: 'Elio Cortes',
+    email: 'elio@gmail.com',
+    phone: '+57 986999845',
+  },
+  {
+    id: '3',
+    name: 'Dino Quispe',
+    email: 'dino@gmail.com',
+    phone: '+591 986999845',
+  },
+];
+
+module.exports = { projects, clients };
