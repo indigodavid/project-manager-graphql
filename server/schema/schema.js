a511d75c6e3ab8f6dbe259025af2b0a5fca6a687
David Vera Castillo <indigodavid@gmail.com>
2023-01-10 16:55:26 -0500

    ✅ Use mongoose Schemas on index

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,11 @@ const {
   GraphQLList,
 } = require('graphql');
 
+// Import Mongoose models
+
+const Project = require('../models/Project');
+const Client = require('../models/Client');
+
 // ClientType
 
 const ClientType = new GraphQLObjectType({
@@ -30,7 +35,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   })
@@ -42,27 +47,27 @@ const RootQuery = new GraphQLObjectType({
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       }
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID }},
       resolve(parent, args) {
-        return clients.find(client => client.id === args.id);
+        return Client.findById(args.id);
       }
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       }
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID }},
       resolve(parent, args) {
-        return projects.find(project => project.id === args.id);
+        return Project.findById(args.id)
       }
     }
   }
