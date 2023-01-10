78566be471ad03e372b45db8cfc9eb474de1cf4c
David Vera Castillo <indigodavid@gmail.com>
2023-01-10 16:25:48 -0500

    ✅ Add Database connection to index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
 const port = process.env.PORT || 5000;
 
 const app = express();
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
   schema,
