9ea55ac892d7f6f70366feace51c70b20c37472a
David Vera Castillo <indigodavid@gmail.com>
2023-01-10 16:24:57 -0500

    ✅ Add database configuration file

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
